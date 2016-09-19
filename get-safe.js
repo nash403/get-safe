6bb832218f0d91c52b4c27b4e4fd72dade05543d
nash403 <nash403@outlook.fr>
2016-09-19 19:50:44 +0200

    moving global variable to local + better export

--- get-safe.js
+++ get-safe.js
@@ -1,9 +1,9 @@
 (function(){
   // Utils
-  function isFnCall(key){
+  const isFnCall = function (key){
     if (typeof key !== 'string') return false;
     return key.slice(-2) === "()";
-  }
+  };
   /*
    * @param {key} string concatenation of nested keys in this form: 'foo.bar.toto'.
    *  You can even call a function if the last key ends with '()'.
@@ -11,22 +11,29 @@
    * @param {...args} a sequence of arguments that may be passed to the function we are calling
    * @return a nested value OR the result of a nested function OR undefined
    */
-  function def_export(key, obj, ...args) {
+  myExport((key, obj, ...args) => {
     let splitted = key.split('.');
     let lastkey = splitted.pop();
-    isFnCallLastkey = isFnCall(lastkey);
+    let isFnCallLastkey = isFnCall(lastkey);
     lastkey = isFnCallLastkey ? lastkey.slice(0,-2) : lastkey;
     let beforelast = splitted.reduce((a,b) => {
       return a && a[b];
     }, obj);
     return beforelast && (typeof beforelast === 'object') && (isFnCallLastkey ? beforelast[lastkey](...args) : beforelast[lastkey]);
-  };
+  }, true, 'getSafe');
 
   // Exports
-  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = def_export;
-  }
-  else {
-    window.getSafe = def_export;
-  }
+  function myExport(exported, isDefault, name) {
+    if (!name) throw "myExport function: <name> mustn't be undefined";
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+      if (isDefault) {
+        module.exports = exported;
+      } else {
+        module.exports[name] = exported;
+      }
+    }
+    else {
+      window[name] = exported;
+    }
+  };
 })();
